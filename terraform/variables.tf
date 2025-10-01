a8f3e76f9f2db8365c53e7793b462979098d1610
Koos van Strien <koos.van.strien@wortell.nl>
2025-10-01 14:36:47 +0200

    Default values added

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group."
+  default     = "demo20251004"
 }
 
 variable "location" {
   type        = string
   description = "The Azure region where resources will be created."
+  default     = "westeurope"
 }
